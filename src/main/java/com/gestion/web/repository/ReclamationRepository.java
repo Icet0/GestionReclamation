9de1e965cb92c38f1397e62dd61199aa5b7974b3
Icet0 <49767388+Icet0@users.noreply.github.com>
2022-05-27 21:19:22 +0200

    Reclamations

--- src/main/java/com/gestion/web/repository/ReclamationRepository.java
+++ src/main/java/com/gestion/web/repository/ReclamationRepository.java
@@ -4,9 +4,11 @@ import com.gestion.web.model.Compte;
 import com.gestion.web.model.Reclamation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReclamationRepository extends JpaRepository<Reclamation, String> {
 
     public Reclamation getReclamationByCompte();
--- src/main/java/com/gestion/web/service/ReclamationService.java
+++ src/main/java/com/gestion/web/service/ReclamationService.java
@@ -1,4 +1,13 @@
 package com.gestion.web.service;
 
+import com.gestion.web.repository.ReclamationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+
 public class ReclamationService {
+
+    @Autowired
+    ReclamationRepository reclamationRepository;
 }
